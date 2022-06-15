2adb943a6ced735e31a1aed9577910d0009b5cc2
KuriWasTaken <74745154+KuriWasTaken@users.noreply.github.com>
2022-06-15 18:56:23 +0100

    Update Loader.lua

--- Verisions/1.1/Loader.lua
+++ Verisions/1.1/Loader.lua
@@ -157,11 +157,13 @@ if game.PlaceId == 606849621 then
 		script.Parent.MouseButton1Click:Connect(function()
 			if sure == false then
 				sure = true
-				loadstring(game:HttpGet("https://raw.githubusercontent.com/KuriWasTaken/Veriax/main/Verisions/" .. currentVersion .. "/Scripts/Jailbreak.lua"))()
 				script.Parent.Parent.Parent:Destroy()
+				script.Parent.Parent.Visible = false
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/KuriWasTaken/Veriax/main/Verisions/" .. currentVersion .. "/Scripts/Jailbreak.lua"))()
 			end
 			if string.find(game:HttpGet("https://raw.githubusercontent.com/KuriWasTaken/Veriax/main/Verisions/CurrentVersion"), currentVersion) then
 				script.Parent.Parent.Parent:Destroy()
+				script.Parent.Parent.Visible = false
 				loadstring(game:HttpGet("https://raw.githubusercontent.com/KuriWasTaken/Veriax/main/Verisions/" .. currentVersion .. "/Scripts/Jailbreak.lua"))()
 				
 			else
