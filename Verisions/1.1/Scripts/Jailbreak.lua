43a8394fd93b5924ed5f8184717b3de53bdd0366
KuriWasTaken <74745154+KuriWasTaken@users.noreply.github.com>
2022-06-17 12:20:28 +0100

    Update Jailbreak.lua

--- Verisions/1.1/Scripts/Jailbreak.lua
+++ Verisions/1.1/Scripts/Jailbreak.lua
@@ -282,18 +282,6 @@ windows.MISC:createToggle("Open All Doors loop", function(a)
     OpenDoors = a
 end)
 
-windows.MISC:createToggle("No Slow Falling", function(a)
-    function deadFunction() --Lazy solution
-        return false 
-    end    
-        
-    
-    if a then
-        require(game:GetService("ReplicatedStorage").Game.Paraglide).IsFlying = deadFunction()
-    else
-        require(game:GetService("ReplicatedStorage").Game.Paraglide).IsFlying = engine.Backups.IsFlying()
-    end
-end)
 
 game:GetService("RunService").RenderStepped:Connect(function()
     local lplr = game.Players.LocalPlayer
