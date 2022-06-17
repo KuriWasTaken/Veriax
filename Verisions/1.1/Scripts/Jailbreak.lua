0f96940f82c0c747ff67c3e75763af09d1bc4eeb
KuriWasTaken <74745154+KuriWasTaken@users.noreply.github.com>
2022-06-17 12:30:49 +0100

    Added No Slow Falling

--- Verisions/1.1/Scripts/Jailbreak.lua
+++ Verisions/1.1/Scripts/Jailbreak.lua
@@ -278,23 +278,20 @@ windows.MISC:createToggle("No Wait", function(a)
     end
 end)
 
+windows.MISC:createToggle("Open All Doors loop", function(a)
+    OpenDoors = a
+end)
+
 windows.MISC:createToggle("No Slow Falling", function(a)
     function deadFunction() --Lazy solution
-        return false 
-    end    
-        
-    
+       return false 
+    end
     if a then
         require(game:GetService("ReplicatedStorage").Game.Paraglide).IsFlying = deadFunction()
     else
         require(game:GetService("ReplicatedStorage").Game.Paraglide).IsFlying = engine.Backups.IsFlying()
     end
 end)
-
-windows.MISC:createToggle("Open All Doors loop", function(a)
-    OpenDoors = a
-end)
-
 
 game:GetService("RunService").RenderStepped:Connect(function()
     local lplr = game.Players.LocalPlayer
